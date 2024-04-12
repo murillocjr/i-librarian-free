44383f5b802ca7572b676c02b05cc71e16cfbe8f
Jorge Murillo <murillo.cjr@gmail.com>
2024-04-12 10:57:21 -0500

    s

--- app/controllers/edit.php
+++ app/controllers/edit.php
@@ -78,7 +78,7 @@ class EditController extends AppController {
         // exec($this->binary->pdfinfo() . ' -enc UTF-8 -f 1 -l 10000 -box ' . escapeshellarg($this->file), $raw);
 
         // $this->queue->release('binary');
-        $myfile = fopen("/tmp/tmp.txt", "w")
+        $myfile = fopen("/tmp/tmp.txt", "w");
         // $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
         $txt = "John Doe\n";
         fwrite($myfile, $txt);
