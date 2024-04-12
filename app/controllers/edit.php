edc68912850080273ca95af7ca62efc565ba72cd
Jorge Murillo <murillo.cjr@gmail.com>
2024-04-12 11:35:11 -0500

    das

--- app/controllers/edit.php
+++ app/controllers/edit.php
@@ -86,7 +86,7 @@ class EditController extends AppController {
         // $this->queue->release('binary');
         // $myfile = fopen("/tmp/tmp.txt", "w");
         $myfile = fopen("/tmp/tmp.txt", "w") or die("Unable to open file!");
-        $txt = serialize($metadata);
+        $txt = var_dump($metadata);
         fwrite($myfile, $txt);
         // close($myfile);
 
